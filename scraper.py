efab767257ff6223e5bd7f1a9b4feb36466f3746
Liana1708 <58812253+Liana1708@users.noreply.github.com>
2020-06-17 16:39:32 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -17,8 +17,12 @@ for li in matchedlinks:
   #Store the text contents of li in a new variable listtext
   listtext=li.text_content()
   print(listtext)
+  #create a dictionary called record
+  record = {}
+   #store it in the 'record' dictionary under the key 'address'
+  record['address'] = listtext
 # # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+scraperwiki.sqlite.save(['address'],record)
 #
 # # An arbitrary query against the database
 # scraperwiki.sql.select("* from data where 'name'='peter'")
